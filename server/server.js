1cb03812bb4b8098a6f1f9c0d54ccdc2713c8099
assegidasefa <assegidassefa7@gmail.com>
2023-06-21 16:31:26 +0300

    adding multi file uploader

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-// Define an API route for file upload
+// Define an API route for file upload // int single file uploading we use upload.single 
 app.post('/api/upload', upload.single('file'), async (req, res) => {
   try {
     const { filename, path } = req.file;
@@ -44,6 +44,34 @@ app.post('/api/upload', upload.single('file'), async (req, res) => {
     res.status(200).json({ message: 'File uploaded successfully' });
   } catch (error) {
     console.error('Error uploading file:', error);
+    res.status(500).json({ error: 'An error occurred during file upload' });
+  }
+});
+
+
+// Define an API route for file upload // for multi file upload we use upload.array
+app.post('/api/multi-upload', upload.array('files'), async (req, res) => {
+  try {
+    const uploadedFiles = req.files;
+
+    // Process each uploaded file
+    for (let i = 0; i < uploadedFiles.length; i++) {
+      const file = uploadedFiles[i];
+      const { originalname, path } = file;
+
+      // Create a new document using the FileModel
+      const fileModel = new FileModel({
+        filename: originalname,
+        filepath: path,
+      });
+
+      // Save the file document to the database
+      await fileModel.save();
+    }
+
+    res.status(200).json({ message: 'Files uploaded successfully' });
+  } catch (error) {
+    console.error('Error uploading files:', error);
     res.status(500).json({ error: 'An error occurred during file upload' });
   }
 });
